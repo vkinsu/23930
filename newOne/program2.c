601df80509f9a950aac7212a6b2456fafd5ad9dc
Ivan Kamenev <i.kamenev@g.nsu.ru>
2024-09-24 14:36:16 +0700

    final task 2

--- newOne/program2.c
+++ newOne/program2.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 extern char *tzname[];
-int main()
-{
+
+int main() {
+
      time_t now;
      struct tm *sp;
 
      setenv("TZ", "PST8PDT", 1);
      tzset();
+     (void) time(&now);
 
-     (void) time( &now );
-
-     printf("%s", ctime( &now ) );
+     printf("%s", ctime(&now));
 
      sp = localtime(&now);
-     printf("%d/%d/%d %d:%02d %s\n",
+     printf("%d/%d/%02d %d:%02d %s\n",
          sp->tm_mon + 1, sp->tm_mday,
-         sp->tm_year+1900, sp->tm_hour,
+         sp->tm_year + 1900, sp->tm_hour,
          sp->tm_min, tzname[sp->tm_isdst]);
+
      exit(0);
 }
