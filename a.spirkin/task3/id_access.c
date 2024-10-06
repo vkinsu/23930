1fc85ed9ab5f99d9e9e7ffe44b7abc4c5c98bff8
Aleksey Spirkin <a.spirkin@g.nsu.ru>
2024-10-06 23:45:14 +0700

    Added task 3.

---
+++ a.spirkin/task3/id_access.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void opening(char *arg) {
+    printf("\tUID: %d\n", getuid());
+    printf("\tEUID: %d\n", geteuid());
+
+    FILE *f = fopen(arg, "r");
+    if (f) {
+        fclose(f);
+    }
+    else {
+        perror("Error on reading the file.");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    opening(argv[1]);
+
+    if (setuid(geteuid()) == -1) {
+        perror("Error on handling UID update.\n");
+    }
+    else {
+        printf("Changed UID to EUID value.\n");
+    }
+
+    opening(argv[1]);
+
+    return 0;
+}
