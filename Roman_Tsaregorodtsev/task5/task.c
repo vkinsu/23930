2c04f2d6f881cc0ca1c8512d34a6b1b4edd1db58
Roman Tsaregorodtsev <r.tsaregorodtsev@g.nsu.ru>
2024-10-15 11:02:26 +0700

    adding 1-7 tasks

---
+++ Roman_Tsaregorodtsev/task5/task.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+typedef struct {
+    long offset;
+    long length;
+} LineInfo;
+
+void build_line_table(const char *filename, LineInfo **table, int *line_count) {
+    int input = open(filename, O_RDONLY);
+    if (input == -1) {
+        perror("error");
+        exit(1);
+    }
+    long offset = 0;
+    long length = 0;
+    char buffer[1];
+    int line_index = 0;
+    LineInfo *temp_table = NULL;
+    while (read(input, buffer, 1) == 1) {
+        if (buffer[0] == '\n') {
+            temp_table = realloc(temp_table, (line_index + 1) * sizeof(LineInfo));
+            temp_table[line_index].offset = offset;
+            temp_table[line_index].length = length;
+            line_index++;
+            offset += length + 1;
+            length = 0;
+        } else {
+            length++;
+        }
+    }
+    if (length > 0) {
+        temp_table = realloc(temp_table, (line_index + 1) * sizeof(LineInfo));
+        temp_table[line_index].offset = offset;
+        temp_table[line_index].length = length;
+        line_index++;
+    }
+    close(input);
+    *table = temp_table;
+    *line_count = line_index;
+}
+
+void print_line(const char *filename, const LineInfo *table, int line_number, int *line_count) {
+    int input = open(filename, O_RDONLY);
+    if (input == -1) {
+        perror("error");
+        exit(1);
+    }
+    if (line_number < 0 || line_number >= *line_count) {
+        printf("Invalid line number\n");
+        close(input);
+        return;
+    }
+    lseek(input, table[line_number].offset, SEEK_SET);
+    char *buffer = (char *)malloc(table[line_number].length + 1);
+    read(input, buffer, table[line_number].length);
+    buffer[table[line_number].length] = '\0';
+    printf("%s\n", buffer);
+    free(buffer);
+    close(input);
+}
+
+int string_to_long(char *str) {
+    for(int i = 0;i<strlen(str);i++)
+    {
+        if(str[i] < '0' || str[i] > '9')
+        {
+            fprintf(stderr, "Invalid input\n");
+            return 0;
+        }
+    }
+    long val;
+    val = atol(str);
+    return (int)val;
+}
+
+
+int main() {
+    const char *filename = "example.txt";
+    LineInfo *table = NULL;
+    int line_count = 0;
+    build_line_table(filename, &table, &line_count);
+    printf("Line table:\n");
+    for (int i = 0; i < line_count; i++) {
+        printf("Line %d: Offset = %ld, Length = %ld\n", i, table[i].offset, table[i].length);
+    }
+    char line_number[256];
+    int num;
+    while (1) {
+        printf("Enter line number: ");
+        scanf("%s", line_number);
+        num = string_to_long(line_number);
+        if (num == 0) {
+            break;
+        }
+        print_line(filename, table, num - 1, &line_count);
+    }
+    free(table);
+    return 0;
+}
---
+++ Roman_Tsaregorodtsev/task7/task.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+char *file_map;
+size_t filesize;
+
+typedef struct {
+    long offset;
+    long length;
+} LineInfo;
+
+void build_line_table(LineInfo **table, int *line_count) {
+    long offset = 0;
+    long length = 0;
+    int line_index = 0;
+    size_t i = 0;
+    LineInfo *temp_table = NULL;
+    while (i != filesize-1) {
+        if (file_map[i] == '\n') {
+            temp_table = (LineInfo *)realloc(temp_table, (line_index + 1) * sizeof(LineInfo));
+            temp_table[line_index].offset = offset;
+            temp_table[line_index].length = length;
+            line_index++;
+            offset += length + 1;
+            length = 0;
+        } else {
+            length++;
+        }
+        i++;
+    }
+    if (length > 0) {
+        temp_table = (LineInfo *)realloc(temp_table, (line_index + 1) * sizeof(LineInfo));
+        temp_table[line_index].offset = offset;
+        temp_table[line_index].length = length;
+        line_index++;
+    }
+    *table = temp_table;
+    *line_count = line_index;
+}
+
+void print_str(LineInfo *table, int line_number, int *line_count){
+    if (line_number < 0 || line_number >= *line_count) {
+        printf("Invalid line number\n");
+        return;
+    }
+    if (line_number < *line_count-1)
+        {for(int i=table[line_number].offset; i<table[line_number+1].offset; i++){
+            printf("%c", file_map[i]);
+        }}
+    else
+        {for(int i=table[line_number].offset; i<=table[line_number].offset + table[line_number].length; i++){
+            printf("%c", file_map[i]);
+        }
+        printf("\n");
+        }
+}
+
+
+void print_file_content() {
+    size_t i = 0;
+    LineInfo *temp_table = NULL;
+    while (i != filesize) {
+        printf("%c", file_map[i]);
+        i++;
+    }
+}
+
+void MyAlarm ()
+{
+    printf("\nTime is up!\n");
+    print_file_content();
+    printf("\n");
+    _Exit (0);
+}
+
+long string_to_long(const char *str) {
+    for(int i = 0;i<strlen(str);i++)
+    {
+        if(str[i] < '0' || str[i] > '9')
+        {
+            fprintf(stderr, "Invalid input\n");
+            return 0;
+        }
+    }
+    long val;
+    val = atol(str);
+    return (int)val;
+}
+
+int main() {
+    LineInfo *table = NULL;
+    int line_count = 0;
+    int input = open("example.txt", O_RDONLY);
+    struct stat f_stat;
+    if (fstat(input, &f_stat) == -1) {
+        perror("error with getting file size");
+        close(input);
+        return 1;
+    }
+    filesize = f_stat.st_size;
+    file_map = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, input, 0);
+    if (file_map == MAP_FAILED) {
+        perror("error with mmap");
+        close(input);
+        return 1;
+    }
+    build_line_table(&table, &line_count);
+    // printf("Line table:\n");
+    // for (int i = 0; i < line_count; i++) {
+    //     printf("Line %d: Offset = %ld, Length = %ld\n", i, table[i].offset, table[i].length);
+    // }
+    char line_number[256];
+    int num;
+    while (1) {
+        printf("You have 5 seconds to write a number\n");
+        signal (SIGALRM, MyAlarm);
+        alarm(5);
+        printf("Enter line number: ");
+        scanf("%s", &line_number);
+        alarm(0);
+        num = string_to_long(line_number);
+        if (num == 0) {
+            break;
+        }
+        print_str(table, num-1, &line_count);
+    }
+    if (munmap(file_map, filesize) == -1)
+        perror("error with munmap");
+    free(table);
+    close(input);
+    return 0;
+}
