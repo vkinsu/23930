c7485c8ba0bde41e27fe6a1e6ce685803aafcbad
Gleb Zhigalov <g.zhigalov@g.nsu.ru>
2024-10-20 15:22:58 +0700

    added 9 and 10

---
+++ Gleb_Zhigalov/task9/lab9.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main() {
+  pid_t pid = fork();
+  if (pid == -1) {
+    perror("Couldn't make process\n");
+    exit(1); 
+  }
+  if (pid == 0) {
+    // Child process
+    execl("lab5", NULL);
+    exit(0);
+  } else {
+    // Parent process 
+    if(wait(NULL) != -1) {
+      printf("\nChild process (pid: %d) finished\n", pid);
+    }
+  }
+  exit(0);
+}
