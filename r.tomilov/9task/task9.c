ec9458661a3c3f66c3778422497476a53a139b59
Roman Tomilov <r.tomilov@g.nsu.ru>
2024-10-22 15:21:29 +0700

    9 and 10 task

---
+++ r.tomilov/9task/task9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() 
+{
+    pid_t pid = fork();
+
+    if (pid < 0) 
+    {
+        perror("fork failed");
+        exit(1);
+    }
+    else if (pid == 0) 
+    {
+        execlp("cat", "cat", "test.txt", (char *)NULL);
+        perror("execlp failed");
+        exit(1);
+    } 
+    else
+    {
+        printf("Parent: I'm printing this while the child is running\n");
+
+        waitpid(pid, NULL, 0);
+
+        printf("Parent: The child has finished, now I can print this\n");
+    }
+
+    return 0;
+}
