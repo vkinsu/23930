c2c1ad51849c91b30c1354fa5fee1437a60a42c0
Mikhail Vays <m.vais@g.nsu.ru>
2024-10-28 13:18:54 +0700

    added task9 folder

---
+++ Mikhail_Vays/task9/task9.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid;
+    int status;
+
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        return 1;
+    } else if (pid == 0) {
+        printf("Подпроцесс (PID=%d) запускает команду cat\n", getpid());
+        
+        execlp("cat", "cat", "longfile.txt", (char *)NULL);
+        
+        perror("execlp failed");
+        exit(1);
+    } else {
+        printf("Родительский процесс (PID=%d): Запущен подпроцесс PID=%d\n", getpid(), pid);
+        
+        waitpid(pid, &status, 0);
+        
+        printf("Родительский процесс: Подпроцесс PID=%d завершился с кодом %d\n", pid, WEXITSTATUS(status));
+    }
+
+    return 0;
+}
