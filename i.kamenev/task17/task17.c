3dbc3492fe6d2a5cf6181240ef02255cf411af20
Ivan Kamenev <i.kamenev@g.nsu.ru>
2024-11-11 23:19:40 +0700

    task17

--- i.kamenev/task17/task17.c
+++ i.kamenev/task17/task17.c
@@ -4,19 +4,19 @@
 #include <termios.h>
 #include <string.h>
 
-#define ERASE 127        // Символ удаления (Backspace)
+#define ERASE 127        // Backspace
 #define KILL 21          // Ctrl-U
-#define CTRL_W 12        // Ctrl-l
+#define CTRL_W 12        // Ctrl-L
 #define CTRL_D 4         // Ctrl-D
-#define CTRL_G 7         // Ctrl-G (звуковой сигнал)
-#define MAX_LINE_LEN 40  // Максимальная длина строки
+#define CTRL_G 7         // Ctrl-G
+#define MAX_LINE_LEN 40  // Max length
 
 void disable_echo_and_canonical(struct termios *old_tio) {
     struct termios new_tio;
     tcgetattr(STDIN_FILENO, old_tio);
     new_tio = *old_tio;
-    new_tio.c_lflag &= ~(ICANON | ECHO | ISIG); // Отключаем канонический режим, эхо и обработку сигналов
-    new_tio.c_cc[VMIN] = 1;              // Немедленная обработка каждого символа
+    new_tio.c_lflag &= ~(ICANON | ECHO | ISIG);
+    new_tio.c_cc[VMIN] = 1;              
     new_tio.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
 }
@@ -30,37 +30,36 @@ void erase_character(char *line, int *pos) {
     if (*pos > 0) {
         (*pos)--;
         line[*pos] = '\0';
-        write(STDOUT_FILENO, "\b \b", 3); // Удаление символа с экрана
+        write(STDOUT_FILENO, "\b \b", 3); 
     } else {
-        write(STDOUT_FILENO, "\a", 1); // Звуковой сигнал при попытке удалить на пустой позиции
+        write(STDOUT_FILENO, "\a", 1);
     }
 }
 
 void kill_line(char *line, int *pos) {
     while (*pos > 0) {
         (*pos)--;
-        write(STDOUT_FILENO, "\b \b", 3); // Удаление символов с экрана
+        write(STDOUT_FILENO, "\b \b", 3); 
     }
     line[0] = '\0';
 }
 
 void erase_word(char *line, int *pos) {
-    // Удаляем все пробелы после слова, если они есть
+    
     while (*pos > 0 && line[*pos - 1] == ' ') {
         erase_character(line, pos);
     }
 
-    // Удаляем символы текущего слова до пробела или начала строки
+    
     while (*pos > 0 && line[*pos - 1] != ' ') {
         erase_character(line, pos);
     }
 
-    // Обновляем отображение строки на экране
-    write(STDOUT_FILENO, "\r", 1); // Переход к началу строки
-    write(STDOUT_FILENO, line, *pos); // Вывод оставшейся части строки
-    write(STDOUT_FILENO, "                                       ", MAX_LINE_LEN - *pos); // Очистка остатка
-    write(STDOUT_FILENO, "\r", 1); // Снова возвращаемся к началу строки
-    write(STDOUT_FILENO, line, *pos); // Вывод строки с удалённым словом
+    write(STDOUT_FILENO, "\r", 1); 
+    write(STDOUT_FILENO, line, *pos); 
+    write(STDOUT_FILENO, "                                       ", MAX_LINE_LEN - *pos);
+    write(STDOUT_FILENO, "\r", 1); 
+    write(STDOUT_FILENO, line, *pos); 
 }
 
 
@@ -68,7 +67,7 @@ int main() {
     struct termios old_tio;
     disable_echo_and_canonical(&old_tio);
 
-    char line[MAX_LINE_LEN + 1] = {0}; // Текущая строка
+    char line[MAX_LINE_LEN + 1] = {0};
     int pos = 0;
 
     printf("Enter text (Ctrl-D to end):\n");
@@ -78,29 +77,29 @@ int main() {
         read(STDIN_FILENO, &c, 1);
 
         if (c == CTRL_D && pos == 0) {
-            break; // Завершение при Ctrl-D в начале строки
+            break;
         } else if (c == ERASE) {
             erase_character(line, &pos);
         } else if (c == KILL) {
             kill_line(line, &pos);
         } else if (c == CTRL_W) {
             erase_word(line, &pos);
-        } else if (c >= 32 && c <= 126) { // Проверка печатных символов
+        } else if (c >= 32 && c <= 126) {
             if (pos < MAX_LINE_LEN) {
                 line[pos++] = c;
-                write(STDOUT_FILENO, &c, 1); // Отображение введенного символа
+                write(STDOUT_FILENO, &c, 1); 
             } 
             else { 
-                // Если достигли конца строки, вставляем перенос строки и начинаем новую строку
-                line[pos] = '\0'; // Завершаем текущую строку
-                write(STDOUT_FILENO, "\n", 1); // Вставка переноса строки
-                pos = 0; // Сброс позиции для новой строки
-                line[pos++] = c; // Добавляем символ в новую строку
-                write(STDOUT_FILENO, &c, 1); // Отображение символа в новой строке
+                
+                line[pos] = '\0'; 
+                write(STDOUT_FILENO, "\n", 1);
+                pos = 0;
+                line[pos++] = c;
+                write(STDOUT_FILENO, &c, 1);
     }
 }
  else {
-            write(STDOUT_FILENO, "\a", 1); // Звуковой сигнал для непечатаемых символов
+            write(STDOUT_FILENO, "\a", 1); 
         }
     }
 
