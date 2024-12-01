11f59f1388a8eec4199ef45b01f20bf60a9d164a
Mikhail Vays <m.vais@g.nsu.ru>
2024-12-01 15:47:50 +0700

    added task32 solution folder

---
+++ Mikhail_Vays/task32/server.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/epoll.h>
+#include <ctype.h>
+#include <errno.h>
+
+#define SOCKET_PATH "./upper_case_socket"
+#define BUFFER_SIZE 256
+#define MAX_EVENTS 10
+
+void to_uppercase(char *str) {
+    for (int i = 0; str[i]; i++) {
+        str[i] = toupper((unsigned char)str[i]);
+    }
+}
+
+int main() {
+    int server_fd, epoll_fd, client_fd;
+    struct sockaddr_un server_addr;
+    struct epoll_event event, events[MAX_EVENTS];
+    char buffer[BUFFER_SIZE];
+
+    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+
+    unlink(SOCKET_PATH);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 5) == -1) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((epoll_fd = epoll_create1(0)) == -1) {
+        perror("epoll_create1");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+        perror("epoll_ctl: server_fd");
+        close(server_fd);
+        close(epoll_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is waiting for connections...\n");
+
+    while (1) {
+        int n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (n_ready == -1) {
+            perror("epoll_wait");
+            break;
+        }
+
+        for (int i = 0; i < n_ready; i++) {
+            if (events[i].data.fd == server_fd) {
+                client_fd = accept(server_fd, NULL, NULL);
+                if (client_fd == -1) {
+                    perror("accept");
+                    continue;
+                }
+
+                printf("New client connected\n");
+
+                event.events = EPOLLIN;
+                event.data.fd = client_fd;
+
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event) == -1) {
+                    perror("epoll_ctl: client_fd");
+                    close(client_fd);
+                }
+            } else {
+                // Handle client data
+                client_fd = events[i].data.fd;
+                ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+
+                if (bytes_read <= 0) {
+                    if (bytes_read == 0) {
+                        printf("Client disconnected\n");
+                    } else {
+                        perror("read");
+                    }
+                    close(client_fd);
+                    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL) == -1) {
+                        perror("epoll_ctl: remove client_fd");
+                    }
+                } else {
+                    buffer[bytes_read] = '\0';
+                    to_uppercase(buffer);
+                    printf("Received from client: %s\n", buffer);
+                }
+            }
+        }
+    }
+
+    close(server_fd);
+    close(epoll_fd);
+    unlink(SOCKET_PATH);
+    printf("Server termination.\n");
+    return 0;
+}
